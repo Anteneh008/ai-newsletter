7b7de2ec2f6b6819970d42cb0c940eb875b6471f
Anteneh Atre <anteneh.atre@gmail.com>
2025-12-17 08:57:16 +0300

    Refactor Home page by optimizing the layout and styles of the Pricing component, ensuring better integration with existing elements for a cohesive user experience.

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,21 @@
+import DashboardHeader from "@/components/dashboard/dashboard-header";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+async function DashboardLayout({ children }: { children: React.ReactNode }) {
+  const { has } = await auth();
+  const hasPaidPlan =
+    (await has({ plan: "pro" })) || (await has({ plan: "starter" }));
+
+  if (!hasPaidPlan) {
+    redirect("/#pricing");
+  }
+  return (
+    <div className="min-h-screen flex flex-col">
+      <DashboardHeader />
+      <main className="flex-1">{children}</main>
+    </div>
+  );
+}
+
+export default DashboardLayout;
